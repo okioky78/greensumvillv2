8378f5796a684867b49c303cbc09e456cd122b5e
okioky78 <okioky78@gmail.com>
2026-03-06 00:40:22 +0900

    Create send-to-sheet.js

---
+++ netlify/functions/send-to-sheet.js
@@ -0,0 +1,25 @@
+exports.handler = async function(event) {
+  try {
+    const body = JSON.parse(event.body);
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        success: true,
+        message: "API 연결 성공",
+        received: body
+      })
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message
+      })
+    };
+  }
+};
