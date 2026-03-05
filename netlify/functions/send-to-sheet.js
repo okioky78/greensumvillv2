09ca4c781ef58e791a8f51afcd26f529b40562ae
okioky78 <okioky78@gmail.com>
2026-03-06 01:06:10 +0900

    Refactor send-to-sheet function for Google Sheets

--- netlify/functions/send-to-sheet.js
+++ netlify/functions/send-to-sheet.js
@@ -1,24 +1,57 @@
+const { google } = require("googleapis");
+
 exports.handler = async (event) => {
   try {
-    const body = event.body ? JSON.parse(event.body) : {};
+
+    const data = JSON.parse(event.body);
+
+    const auth = new google.auth.JWT(
+      process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      null,
+      process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      ["https://www.googleapis.com/auth/spreadsheets"]
+    );
+
+    const sheets = google.sheets({ version: "v4", auth });
+
+    const values = [[
+      data.date,
+      data.amount,
+      data.card,
+      data.approval,
+      data.business,
+      data.student,
+      data.memo,
+      data.branch,
+      data.payment
+    ]];
+
+    await sheets.spreadsheets.values.append({
+      spreadsheetId: process.env.GOOGLE_SHEET_ID,
+      range: "Sheet1!A:I",
+      valueInputOption: "USER_ENTERED",
+      requestBody: {
+        values
+      }
+    });
 
     return {
       statusCode: 200,
-      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         ok: true,
-        message: "send-to-sheet function OK",
-        received: body,
-      }),
+        message: "구글 시트 저장 완료"
+      })
     };
-  } catch (e) {
+
+  } catch (error) {
+
     return {
-      statusCode: 400,
-      headers: { "Content-Type": "application/json" },
+      statusCode: 500,
       body: JSON.stringify({
         ok: false,
-        error: e.message,
-      }),
+        error: error.message
+      })
     };
+
   }
 };
